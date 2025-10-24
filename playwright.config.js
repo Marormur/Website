68eedb946e6a39f3dfbf170040e55605a7ac2103
Marvin Temmen <github@marvintemmen.de>
2025-10-24 23:32:17 +0200

    fix: update Playwright baseURL to match dev server port 5173

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: 'list',
     use: {
-        baseURL: 'http://127.0.0.1:3000',
+        baseURL: 'http://127.0.0.1:5173',
         trace: 'on-first-retry',
         screenshot: 'only-on-failure',
         video: 'retain-on-failure'
